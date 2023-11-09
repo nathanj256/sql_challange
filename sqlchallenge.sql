c2b1455c889bb96d4a8d597e00a7ba2246231b45
nathanj256 <146050988+nathanj256@users.noreply.github.com>
2023-11-09 00:38:20 -0600

    Add files via upload

---
+++ sqlchallenge.sql
@@ -0,0 +1,48 @@
+CREATE TABLE "departments" (
+  "dept_no" varchar,
+  "dept_name" varchar
+);
+
+CREATE TABLE "department_employees" (
+  "emp_no" VARCHAR,
+  "dept_no" varchar
+);
+
+CREATE TABLE "employees" (
+  "emp_no" varchar,
+  "emp_title_id" varchar,
+  "firt_date" date,
+  "first_Name" varchar,
+  "last_Name" varchar,
+  "sex" varchar(20),
+  "hire_date" date
+);
+
+CREATE TABLE "Salary" (
+  "emp_no" VARCHAR,
+  "Salary" int
+);
+
+CREATE TABLE "department_manager" (
+  "emp_no" VARCHAR,
+  "dept_no" int
+);
+
+CREATE TABLE "titles" (
+  "title_id" int,
+  "title" varchar
+);
+
+ALTER TABLE "department_employees" ADD FOREIGN KEY ("emp_no") REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "employees" ADD FOREIGN KEY ("emp_no") REFERENCES "department_employees" ("emp_no");
+
+ALTER TABLE "Salary" ADD FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD FOREIGN KEY ("title_id") REFERENCES "employees" ("emp_title_id");
+
+ALTER TABLE "departments" ADD FOREIGN KEY ("dept_no") REFERENCES "department_manager" ("dept_no");
+
+ALTER TABLE "department_manager" ADD FOREIGN KEY ("emp_no") REFERENCES "Salary" ("emp_no");
+
+ALTER TABLE "employees" ADD FOREIGN KEY ("emp_no") REFERENCES "employees" ("first_Name");
